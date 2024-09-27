0214d4d42bd1b17507ca55140686c5e867df45b3
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-09-27 12:37:59 +0100

    Update and rename Hello to Hello.java

---
+++ Chapter1/Hello.java
@@ -0,0 +1,17 @@
+package com.mycompany.chapter1;
+
+/*
+ Purpose: Output Hello
+ @author Darren Stones
+ Date: 20 Sept 2024
+*/
+
+//Class Declaration
+public class Chapter1 {
+    
+    //Main method 
+    public static void main(String[] args) {
+        //Output Hello World 
+        System.out.println("Hello World!");
+    } //end main 
+}  //end class
