6a837a98d67a6d8dc8123621d8ba5596e3584f1b
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-10-01 13:09:58 +0100

    Create Assignment.java

---
+++ Chapter2/Assignment.java
@@ -0,0 +1,22 @@
+package com.mycompany.chapter2;
+
+public class Assignment {
+
+
+    public static void main(String[] args) {
+        // Assignment and Equality Operators 
+        
+        //Addition and Assignment Operator +=
+        int a = 10;
+        int b = 20;
+        // a = a + b;
+        a += b;  // 30 = 10+20 
+        System.out.println("Result 1: "+a);
+        
+        //Equality Operator == 
+        //Checking for equality (comparison) 
+        System.out.println("Is A equal to B: "+ (a==b));
+        
+        
+    } //end main
+} //end class
