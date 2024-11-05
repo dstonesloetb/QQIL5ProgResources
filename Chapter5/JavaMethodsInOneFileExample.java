d355c0c81cd10a91d2a002741a2887c223ea1d34
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-11-05 14:45:17 +0000

    Update JavaMethodsInOneFileExample.java

--- Chapter5/JavaMethodsInOneFileExample.java
+++ Chapter5/JavaMethodsInOneFileExample.java
@@ -1,68 +1,86 @@
+package com.mycompany.chapter5;
+
 public class JavaMethodsInOneFileExample {
-  
-  public static void main(String[] args) {
     
-    System.out.println("Hello from main!");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //Calling myFunctionOne 
+        //No Input, No Return
+        myFunctionOne();
+        
+        //Calling myFuntionTwo
+        //No Input, Return an int
+        
+        //Calling function 2 from the println function
+        System.out.println(myFunctionTwo());
+        
+        //Calling function 2 and storing the
+        //returned result from the function 
+        int result = myFunctionTwo();
+        System.out.println("Result: " + result);
+        
+        
+        //Call function 3 and pass in an int value
+        int x = 20;
+        myFunctionThree(40);
+        myFunctionThree(x);  //preferred approach
+        // myFunctionThree();  FOUND NO ARGUMENTS 
+        
+        //call myFunctionFour - inputs , return 
+        int speedCar = 130;
+        int speedLimitRoad = 50;
+        //boolean issueTicket = myFunctionFour(130, 50);
+        boolean issueTicket = myFunctionFour(speedCar, speedLimitRoad);
+        System.out.println("Issue ticket: "+issueTicket);
+        
+        
+        System.out.println("Return to main method...");
+        
+    } //end main 
 
-
-    //call myFunctionOne
-    //no input, no return 
-    myFunctionOne();   
-
+    //*****************************************
     
-    //call myFunctionTwo
-    System.out.println(myFunctionTwo());  
+    //Function 1 - has no input, no return
+    //displayMenu()
+    public static void myFunctionOne() {
+        System.out.println("Display Menu...");
+    } //end of myFunctionOne
 
-    //no input - return int
-    int result = myFunctionTwo();  
-    System.out.println("Num vowels: "+result);
-
+    //******************************************
     
-    //call myFunctionThree - input , no return 
-    int x = 20;
-    myFunctionThree(100);
-    myFunctionThree(x);
-
+    //Function 2 - no input, returning an int
+    public static int myFunctionTwo() {
+        //local variable
+        int numOfVowels = 0; 
+        
+        //code to count vowels in a string
+        
+        return numOfVowels;
+        
+    } //end myFunctionTwo
     
-    //call myFunctionFour - inputs , return 
-    boolean issueTicket = myFunctionFour(130, 50);
-    System.out.println("Issue ticket: "+issueTicket);
-   
+    //*****************************************
     
-  } //end main 
-
-  
-  // Scenario 1: Function - No input/No return 
-  public static void myFunctionOne() {
-    System.out.println("Output Menu");
-  } //end myFunctionOne
-
-
-  
-    // Scenario 2: Function -No input/Yes return 
-  public static int myFunctionTwo() {
-    //create a local variable
-    int numVowels=0;
-    //code to count vowels ..... 
-    return numVowels;
-  } //end myFunctionTwo
-
-
-  
-  //Scenario 3 - Input Yes/No Return
-  public static void myFunctionThree(int num)
-  {
-    int num2 = 100;
-    System.out.println(num+num2);
-  } //end myFunctionThree
-
-
-  
-  //Scenario 4 - Yes Input, Yes Return
-  public static boolean myFunctionFour(int speed, int speedLimit){
-    boolean result = (speed>speedLimit) ? true : false ;  
-    return result;
-  } //end myFunctionFour
-
-   
+    //Function 3 - input, no return
+    //This function has one parameter variable num
+    public static void myFunctionThree(int num)
+    {
+        //declare and initialise a local variable
+        int num2 = 100; 
+        int result = num + num2; 
+        System.out.println(result);
+    } //end myFunctionThree
+    
+    //***********************************************************
+    //Scenario 4 - Yes Input, Yes Return
+    //issueSpeedingTicket(int speed, int speedLimit)  2 int parameter vars
+    public static boolean myFunctionFour(int speed, int speedLimit)
+    {
+                         //  130 > 50 -> true      TRUE : FALSE
+        boolean result = (speed>speedLimit)      ? true : false ;  
+        return result;
+    }  //end myFunctionFour
+    
+    
 } //end class 
