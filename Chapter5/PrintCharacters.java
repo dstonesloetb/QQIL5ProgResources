b91d616a5e29fe4cd13a7f4aa573d37e31f063a0
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-11-12 12:40:32 +0000

    Update PrintCharacters.java

--- Chapter5/PrintCharacters.java
+++ Chapter5/PrintCharacters.java
@@ -8,8 +8,11 @@ public class PrintCharacters {
         String str = "Hello World!";
         
         //Get the length of the string
-        int lenStr = str.length();
+        int lenStr = str.length();  //len=12
         System.out.println("Length of string: "+lenStr);
+        
+        //Index for the string goes from 0 to the len-1
+        //index for the current string ranges from 0 to 11 ie. (12-1)
         
         //Get the first character in the string
         char firstChar = str.charAt(0);
@@ -17,7 +20,7 @@ public class PrintCharacters {
         
         //Display all the characters in the string one by one
         //Show on the same line seperated by a space
-        //Index for string is 0 to 14 inclusive   i.e. str.length-1 (15-1)
+        //Index for string is 0 to 11 inclusive   i.e. str.length-1 (12-1)
                     // i<str.length()
         for (int i=0; i<=str.length()-1; i++)
         {
